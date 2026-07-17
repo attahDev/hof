beb5922e3110d169e716450fd59d28d809d8b80b
attahDev <attahumana007@gmail.com>
2026-07-17 10:23:37 +0100

    Wrap InducteesGrid in Suspense for lazy loading

--- app/dashboard/inductees/page.tsx
+++ app/dashboard/inductees/page.tsx
@@ -1,15 +1,14 @@
-+import { Suspense } from "react";
- import HallOfFameHeroBanner from "../inductees/component/HallOfFameHeroBanner"
- import InducteesGrid from "./component/InducteesGrid";
- 
- export default function HallOfFameRoutePage() {
-   return (
-     <main className="min-h-screen w-full bg-[#F5EBE1] p-[30px]">
-       <HallOfFameHeroBanner />
--      <InducteesGrid />
-+      <Suspense fallback={null}>
-+        <InducteesGrid />
-+      </Suspense>
-     </main>
-   );
- }
+import { Suspense } from "react";
+import HallOfFameHeroBanner from "../inductees/component/HallOfFameHeroBanner"
+import InducteesGrid from "./component/InducteesGrid";
+
+export default function HallOfFameRoutePage() {
+  return (
+    <main className="min-h-screen w-full bg-[#F5EBE1] p-[30px]">
+      <HallOfFameHeroBanner />
+      <Suspense fallback={null}>
+        <InducteesGrid />
+      </Suspense>
+    </main>
+  );
+}
