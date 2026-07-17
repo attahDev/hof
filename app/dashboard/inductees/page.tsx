4849cbdcce80d5f66989d66677aa195b00bebd9d
attahDev <attahumana007@gmail.com>
2026-07-17 10:22:02 +0100

    Update page.tsx

--- app/dashboard/inductees/page.tsx
+++ app/dashboard/inductees/page.tsx
@@ -1,11 +1,15 @@
-import HallOfFameHeroBanner from "../inductees/component/HallOfFameHeroBanner"
-import InducteesGrid from "./component/InducteesGrid";
-
-export default function HallOfFameRoutePage() {
-  return (
-    <main className="min-h-screen w-full bg-[#F5EBE1] p-[30px]">
-      <HallOfFameHeroBanner />
-      <InducteesGrid />
-    </main>
-  );
-}
++import { Suspense } from "react";
+ import HallOfFameHeroBanner from "../inductees/component/HallOfFameHeroBanner"
+ import InducteesGrid from "./component/InducteesGrid";
+ 
+ export default function HallOfFameRoutePage() {
+   return (
+     <main className="min-h-screen w-full bg-[#F5EBE1] p-[30px]">
+       <HallOfFameHeroBanner />
+-      <InducteesGrid />
++      <Suspense fallback={null}>
++        <InducteesGrid />
++      </Suspense>
+     </main>
+   );
+ }
